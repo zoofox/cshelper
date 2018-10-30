3e35af669170713d6aa4e211b651d15bbc8be097
zoofox <457033517@qq.com>
2018-10-30 22:15:33 +0800

    read

--- miniprogram/pages/read/read.js
+++ miniprogram/pages/read/read.js
@@ -166,9 +166,10 @@ Page({
     this.data.audio.title = '触手直播助手';
     this.data.audio.epname = this.data.roomId;
     this.data.audio.singer = '房间号'
-    this.data.audio.coverImgUrl = 'https://kascdn.kascend.com/jellyfish/avatar/109474876/1509364203903.jpg'
+    this.data.audio.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
     // 设置了 src 之后会自动播放
     //this.data.audio.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+    // this.data.audio.src ='http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
     this.data.audio.onEnded((res) => {
       if (this.data.isReading) {
         console.log('read end')
@@ -195,6 +196,9 @@ Page({
     console.log(src);
     console.log(this.data.audio);
     this.data.audio.src = src;
+    this.data.audio.title = '触手直播助手';
+    this.data.audio.epname = this.data.roomId;
+    this.data.audio.singer = '房间号'
   },
   stop(){
     this.data.audio.stop();
